74a271e8dd85426e4173a64b9ddbc1b8879d1fce
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-02 14:49:37 -0400

    build_radxa02.sh: Use proper SM1 configs

--- build_radxa02.sh
+++ build_radxa02.sh
@@ -2,12 +2,12 @@
 
 if [ "${FORCE_RECOVERY}" == "true" ]
 then
-./build.sh android-tv-13.0.0_r1-recovery-only g12a_radxa0_v1
+./build.sh android-tv-13.0.0_r1-recovery-only sm1_radxa02_v1
 elif [ "${CONSOLE_ENABLED}" == "true" ]
 then
-./build.sh android-tv-13.0.0_r1-console g12a_radxa0_v1
+./build.sh android-tv-13.0.0_r1-console sm1_radxa02_v1
 else
-./build.sh android-tv-13.0.0_r1 g12a_radxa0_v1
+./build.sh android-tv-13.0.0_r1 sm1_radxa02_v1
 fi
 ./generate-bins-new.sh fip-radxa02-220427 out/u-boot/build/u-boot.bin
 
